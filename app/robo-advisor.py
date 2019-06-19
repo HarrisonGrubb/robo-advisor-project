5c620c3ff1ce048274083c0317133f4c11385b76
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-18 21:41:46 -0400

    looping writer

--- app/robo-advisor.py
+++ app/robo-advisor.py
@@ -77,11 +77,12 @@ csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", 'data.csv'
 with open(csv_file_path, "w") as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames = ['stock_symbol', 'time_stamp', 'open', 'recent_high', 'recent_low', 'close', 'volume'])
     writer.writeheader()
-    writer.writerow({
-        'stock_symbol' : symbol,
-        'time_stamp': request_time, 
-        'open' : latest_open, 
-        'recent_high' : highest_price, 
-        'recent_low': lowest_price, 
-        'close' : latest_close, 
-        'volume': latest_volume})
+    for date in dates:
+        writer.writerow({
+            'stock_symbol' : symbol,
+            'time_stamp': request_time, 
+            'open' : latest_open, 
+            'recent_high' : highest_price, 
+            'recent_low': lowest_price, 
+            'close' : latest_close, 
+            'volume': latest_volume})
