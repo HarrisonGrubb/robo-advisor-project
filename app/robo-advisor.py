1fe6bfbf79d5f6e68b212400b1d4c86fdd100ef4
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-18 20:38:09 -0400

    closing price and formats

--- app/robo-advisor.py
+++ app/robo-advisor.py
@@ -3,6 +3,10 @@ import os
 from dotenv import load_dotenv
 import json
 import pandas as pd 
+import datetime
+
+def to_usd(my_price):
+    return "(${:,.2f})".format(my_price)
 
 load_dotenv()
 
@@ -23,21 +27,21 @@ response = requests.get(request_url)
 # print(response.text)
 
 parsed_response = json.loads(response.text)
+request_time = datetime.datetime.now().strftime("%b %d %Y %H:%M:%S")
 
 last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
-
+latest_close = parsed_response['Time Series (Daily)']['2019-06-18']['4. close']
 
 # breakpoint()
-
 
 print("-------------------------")
 print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT: {request_time}")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
