9e0787e3be494d09c43c4c41ad4cd0180aa37924
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-18 21:36:06 -0400

    working writer...now to not overwrite

--- app/robo-advisor.py
+++ app/robo-advisor.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import json
+import csv 
 import pandas as pd 
 import datetime
 
@@ -16,7 +17,6 @@ base_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=
 symbol = input('give me a ticker mofo ')
 key = '&apikey=' + ALPHAVANTAGE_API_KEY
 
-# request_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo'
 
 request_url = base_url + symbol + key
 
@@ -35,6 +35,8 @@ latest_day = dates[0] # sort list so latest day is first assumes latest day is f
 
 last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
 latest_close = tsd[latest_day]['4. close']
+latest_open = tsd[latest_day]['1. open']
+latest_volume = tsd[latest_day]['5. volume']
 
 high_list = []
 low_list = []
@@ -65,7 +67,21 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print('WRTING TO CSV!')
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
 
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", 'data.csv')
 
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames = ['stock_symbol', 'time_stamp', 'open', 'recent_high', 'recent_low', 'close', 'volume'])
+    writer.writeheader()
+    writer.writerow({
+        'stock_symbol' : symbol,
+        'time_stamp': request_time, 
+        'open' : latest_open, 
+        'recent_high' : highest_price, 
+        'recent_low': lowest_price, 
+        'close' : latest_close, 
+        'volume': latest_volume})
